c3e6d30e5eeddc91765f153933c4356efc40c495
Inter <enacheandrei20@yahoo.com>
2024-01-03 11:51:15 +0000

    ✨ feat: add package version command (#1)

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
 const { program } = require('commander');
-const badges = require('./badges'); // Import the badges file
+const badges = require('./badges');
+const packageInfo = require('./package.json');
 
-// Function to format the category name for better readability
 function formatCategoryName(category) {
     return category.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
 }
 
 program
-    .version('1.0.0')
-    .description('Generate markdown badges for different categories.');
+    .version('1.1.0')
+    .description('A command line tool to find Shield.io badges.');
 
-    program
+program
     .arguments('<category> <badgeName>')
     .action((category, badgeName) => {
         const formattedCategory = formatCategoryName(category);
@@ -22,15 +22,34 @@ program
             if (badge) {
                 console.log(`Badge found for "${formattedCategory}" category:`, badge);
             } else {
-                console.log(``)
+                console.log(``);
                 console.log(`Badge not found for "${formattedCategory}" category with the specified name.`);
                 console.log(``);
                 console.log(`If your name has a space, try entering a dash.`);
-                console.log(``)
                 console.log(`eg. applemusic -> apple-music`);
             }
         } else {
             console.log(`Category "${formattedCategory}" not found.`);
         }
     });
+
+program
+    .command('fund')
+    .description('Shows. funding information for the project.')
+    .action(() => {
+        console.log('');
+        console.log('If you would like, you can donate to me here:');
+        console.log('');
+        console.log('GitHub Sponsors: https://github.com/sponsors/inttter');
+        console.log('');
+    });
+
+    program
+    .command('version')
+    .description('Displays the current version.')
+    .action(() => {
+        console.log(`${packageInfo.version}`);
+    });
+
+
 program.parse(process.argv);

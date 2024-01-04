53474e6fec62aaffd0ca8efadafd4706eb62f7f2
Inter <enacheandrei20@yahoo.com>
2024-01-04 18:43:57 +0000

    ✨ feat: add support for HTML (#20)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const { program } = require('commander');
 const axios = require('axios');
+const chalk = require('chalk');
 const badges = require('./badges');
 const packageInfo = require('./package.json');
 
@@ -10,29 +11,40 @@ function formatCategoryName(category) {
 }
 
 program
-    .version('1.3.1')
+    .version('2.0.0')
     .description('A command line tool to find Shield.io badges.');
 
     program
     .arguments('<category> <badgeName>')
-    .action((category, badgeName) => {
-        const formattedCategory = formatCategoryName(category);
-        const categoryData = badges[category.toLowerCase()];
-        if (categoryData) {
-            const badge = categoryData[badgeName.toLowerCase()];
-            if (badge) {
-                console.log(`Badge found for "${formattedCategory}" category:`, badge);
-            } else {
-                console.log(``);
-                console.log(`Badge not found for "${formattedCategory}" category with the specified name.`);
-                console.log(``);
-                console.log(`If your name has a space, try entering a dash.`);
-                console.log(`eg. applemusic -> apple-music`);
-                console.log(``);
-            }
+    .option('--html', 'Generate HTML badge code')
+    .action((category, badgeName, options) => {
+      const formattedCategory = formatCategoryName(category);
+      const categoryData = badges[category.toLowerCase()];
+      if (categoryData) {
+        const badge = categoryData[badgeName.toLowerCase()];
+        if (badge) {
+          if (options.html) {
+            // extracts badge link
+            const badgeLink = badge.match(/\(([^)]+)\)/)[1];
+            // extracts badge namen
+            const badgeAlt = badge.match(/\[([^)]+)\]/)[1];
+            // formats the HTML
+            const htmlBadge = `<img src="${badgeLink}" />`;
+            console.log(htmlBadge);
+          } else {
+            console.log(chalk.hex('#00FF00')(`Badge found for "${formattedCategory}" category:`, badge));
+          }
         } else {
-            console.log(`Category "${formattedCategory}" not found.`);
+          console.log(``);
+          console.log(chalk.hex('FF0000')(`Badge not found for "${formattedCategory}" category with the specified name.`));
+          console.log(``);
+          console.log(`If your name has a space, try entering a dash.`);
+          console.log(`eg. applemusic -> apple-music`);
+          console.log(``);
         }
+      } else {
+        console.log(`Category "${formattedCategory}" not found.`);
+      }
     });
 
     program
@@ -73,7 +85,7 @@ program
     .alias('upd')
     .description('Checks for updates to the CLI.')
     .action(async () => {
-      console.log('Checking for updates...');
+      console.log(chalk.blueBright('Checking for updates...'));
       try {
         const response = await axios.get('https://registry.npmjs.org/mdbadges-cli');
         const latest = response.data['dist-tags'].latest;
@@ -102,6 +114,7 @@ program
                 console.log(`- ${badge}`);
             });
             console.log(`\nTo get the code for a badge, type 'mdb ${category} <badgeName>'.`);
+            console.log(`If you want the HTML version of a badge, type 'mdb --html ${category} <badgeName>'.`);
         } else {
             console.log(`Category "${formattedCategory}" not found.`);
         }

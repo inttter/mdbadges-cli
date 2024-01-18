5b0e61cac0978d14a227deb357e0916843409667
Inter <enacheandrei20@yahoo.com>
2024-01-18 17:34:14 +0000

    🐛 fix: fix about command not displaying latest version correctly
    * 💡added some comments too

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ const badges = require("./badges");
 const packageInfo = require("./package.json");
 const { searchCommand } = require("./commands/search");
 
-function formatCategoryName(category) {
+function formatCategoryName(category) { // function for formatting names of categorys (see 'commands/search.js' for an example)
   return category
     .split("-")
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join(" ");
 }
 
-function formatBadgeName(badgeName) {
+function formatBadgeName(badgeName) { // formats badge names for outputs
   const formattedBadgeName = badgeName
     .split("-")
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
@@ -31,7 +31,7 @@ function formatBadgeName(badgeName) {
 program.version("3.3.0").description("A package to find Shields.io badges.");
 
 program
-  .arguments("<category> [badgeNames...]")
+  .arguments("<category> [badgeNames...]") // [badgeNames...] allows for more than one badge
   .description("Displays Markdown for specified badge in a category.")
   .option("--html", "Generates the HTML version of the badge code") // tag that toggles html code
   .option("-s, --style <badgeStyle>", "Toggles badge style") // tags that toggle badge style
@@ -41,7 +41,7 @@ program
 
     if (categoryData) {
       console.log();
-      console.log(gradient.cristal(`Badge found:`)); // Move this line outside the forEach loop
+      console.log(gradient.cristal(`Badge found:`));
       badgeNames.forEach((badgeName) => {
         const formattedBadgeName = badgeName.toLowerCase();
         const foundBadge = Object.keys(categoryData).find(
@@ -51,17 +51,17 @@ program
 
         if (badge) {
           if (options.html) {
-            // Extracts the badge link and its name
+            // extracts the badge link and its name
             const badgeLink = badge.match(/\(([^)]+)\)/)[1];
             const badgeAlt = badge.match(/\[([^)]+)\]/)[1];
-            // Formats the HTML code with style option
+            // formats the HTML code with its style option
             const styleOption = options.style ? `&style=${options.style}` : "";
             const htmlBadge = `<img src="${badgeLink}${styleOption}" />`;
             console.log(chalk.hex("#FFBF00").bold(htmlBadge));
           } else {
             let badgeStyle = "flat"; // flat is the default style if one is not specified
             if (options.style) {
-              // Checks if the provided style matches one of the accepted styles
+              // provided style must matche one of these styles
               const styles = [
                 "flat",
                 "flat-square",
@@ -99,7 +99,7 @@ program
           console.log(chalk.hex("#289FF9")(`e.g., applemusic -> apple-music`));
         }
       });
-      console.log(); // Add an empty line after the loop for better formatting
+      console.log();
     } else {
       console.log(chalk.hex("#FF0000")(`That category could not be found.`));
       console.log(
@@ -205,7 +205,7 @@ program
   .alias("find")
   .description("Displays badges available in a category.")
   .action(() => {
-    searchCommand(badges);
+    searchCommand(badges); // command has been moved to 'commands/search.js', this fetches that file and uses the code accordingly
   });
 
 program
@@ -344,35 +344,46 @@ program
   program
   .command('about')
   .description('Displays general information about the package.')
-  .action(() => {
-    // prettier-ignore
-    console.log()
-    console.log(gradient.vice('                 _  _                 _                                 _  _ '));
-    console.log(gradient.vice('                | || |               | |                               | |(_) '));
-    console.log(gradient.vice('  _ __ ___    __| || |__    __ _   __| |  __ _   ___  ___  ______  ___ | | _ '));
-    console.log(gradient.vice(' | \'_ ` _ \\  / _` || \'_ \\  / _` | / _` | / _` | / _ \\/ __||______|/ __|| || |'));
-    console.log(gradient.vice(' | | | | | || (_| || |_) || (_| || (_| || (_| ||  __/\\__ \\       | (__ | || |'));
-    console.log(gradient.vice(' |_| |_| |_| \\__,_||_.__/  \\__,_| \\__,_| \\__, | \\___||___/        \\___||_||_|'));
-    console.log(gradient.vice('                                          __/ |                              '));
-    console.log(gradient.vice('                                         |___/                               '));
-    console.log()
-    console.log(gradient.vice('                      A package to find Shields.io badges.                      '));    
-    console.log(gradient.vice('                            https://cli.inttter.com                            '))
-    console.log()
-    console.log(gradient.retro(`Latest Version: ${packageInfo.version}`));
-    
-    const userPackageVersion = packageInfo.version;
-    console.log(gradient.summer(`You are currently using version: ${userPackageVersion}`));
-    console.log()
-    console.log(gradient.atlas(`If you need to, update by running 'mdb update' or 'mdb upd'`));
-    console.log()
-    console.log(gradient.atlas(`License: https://mit-license.org/`));
-    console.log()
-    console.log(gradient.atlas(`Type 'mdb -h' to view the available list of commands`));
-    console.log(gradient.atlas(`Type 'mdb fund' if you'd like to donate`));
-    console.log()
-    console.log(gradient.atlas(`Report any issues on GitHub: https://github.com/inttter/mdbadges-cli/issues`));
-    console.log(gradient.atlas(`To add a badge, visit this repository: https://github.com/inttter/md-badges`));
+  .action(async () => {
+    async function displayAboutInfo() {
+      try {
+        const response = await axios.get(`https://registry.npmjs.org/mdbadges-cli`);
+        const latestVersion = response.data["dist-tags"].latest;
+
+        console.log();
+        console.log(gradient.vice('                 _  _                 _                                 _  _ '));
+        console.log(gradient.vice('                | || |               | |                               | |(_) '));
+        console.log(gradient.vice('  _ __ ___    __| || |__    __ _   __| |  __ _   ___  ___  ______  ___ | | _ '));
+        console.log(gradient.vice(' | \'_ ` _ \\  / _` || \'_ \\  / _` | / _` | / _` | / _ \\/ __||______|/ __|| || |'));
+        console.log(gradient.vice(' | | | | | || (_| || |_) || (_| || (_| || (_| ||  __/\\__ \\       | (__ | || |'));
+        console.log(gradient.vice(' |_| |_| |_| \\__,_||_.__/  \\__,_| \\__,_| \\__, | \\___||___/        \\___||_||_|'));
+        console.log(gradient.vice('                                          __/ |                              '));
+        console.log(gradient.vice('                                         |___/                               '));
+        console.log();
+        console.log(gradient.vice('                      A package to find Shields.io badges.                      '));    
+        console.log(gradient.vice('                            https://cli.inttter.com                            '));
+        console.log();
+        console.log(gradient.retro(`Latest Version: ${latestVersion}`));
+
+        const userPackageVersion = packageInfo.version;
+        console.log(gradient.summer(`You are on: ${userPackageVersion}`));
+        console.log();
+        console.log(gradient.atlas(`If you need to, update by running 'mdb update' or 'mdb upd'`));
+        console.log();
+        console.log(gradient.atlas(`License: https://mit-license.org/`));
+        console.log();
+        console.log(gradient.atlas(`Type 'mdb -h' to view the available list of commands`));
+        console.log(gradient.atlas(`Type 'mdb fund' if you'd like to donate`));
+        console.log();
+        console.log(gradient.atlas(`Report any issues on GitHub: https://github.com/inttter/mdbadges-cli/issues`));
+        console.log(gradient.atlas(`To add a badge, view the contributing guidelines: https://github.com/inttter/mdbadges-cli/blob/main/CONTRIBUTING.md`));
+      } catch (error) {
+        console.error(`Error fetching latest version: ${error.message}`);
+      }
+    }
+
+    // Call the function
+    await displayAboutInfo();
   });
 
 program
@@ -514,14 +525,14 @@ program
 
     const badge = categoryData[foundBadge];
 
-    // Check if badge is defined and the regex match is successful
+    // this checks if the badge is defined + if the regex match is successful
     const badgeLinkMatch = badge.match(/\(([^)]+)\)/);
     if (!badgeLinkMatch || !badgeLinkMatch[1]) {
       console.log(chalk.hex("#FF0000")("Badge link not found in the expected format."));
       return;
     }
 
-    // Extracts the badge link and its name
+    // extracts the badge link and its name
     const badgeLink = badgeLinkMatch[1];
     const badgeAlt = badge.match(/\[([^)]+)\]/)[1];
 
@@ -533,7 +544,7 @@ program
     return;
   }
 
-    // Read the existing content of the file
+    // reads the existing content of the file
     const fs = require("fs");
     let fileContent = "";
     try {
@@ -543,7 +554,7 @@ program
       return;
     }
 
-    // Append the badge to the file
+    // adds the badge to the file
     try {
       fs.appendFileSync(filePath, `\n${badgeMarkdown}\n`, "utf8");
       console.log()

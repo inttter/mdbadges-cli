7e190916279ac8c45e22c106f1a1cf562e80ebbe
Inter <me@inttter.com>
2024-02-09 17:21:29 +0000

    💎 style: improve visibility of links
    Signed-off-by: Inter <me@inttter.com>

--- src/index.js
+++ src/index.js
@@ -215,8 +215,8 @@ program
       setTimeout(() => {
         console.log();
         console.log(c.yellow('Hasn\'t opened yet? Try one of these two links:'));
-        console.log(c.dim(`https://github.com/inttter/md-badges`));
-        console.log(c.dim(`https://mdbdocs.inttter.com/content/badges`));
+        console.log(c.magenta(`https://github.com/inttter/md-badges`));
+        console.log(c.magenta(`https://mdbdocs.inttter.com/content/badges`));
       }, 5000); // 5 seconds
     }
   });
@@ -781,7 +781,7 @@ program
     setTimeout(() => {
       console.log();
       console.log(c.yellow('Hasn\'t opened in your browser? Try clicking on the link below:'));
-      console.log(c.dim(`https://docs.mdbcli.xyz/`));
+      console.log(c.magenta(`https://docs.mdbcli.xyz/`));
     }, 5000) // 5 seconds
   }
 });
@@ -819,7 +819,7 @@ program
       setTimeout(() => {
         console.log();
         console.log(c.yellow('Hasn\'t opened in your browser? Try clicking on the link below:'));
-        console.log(c.dim(`https://github.com/inttter/mdbadges-cli/releases/latest`));
+        console.log(c.magenta(`https://github.com/inttter/mdbadges-cli/releases/latest`));
       }, 5000) // 5 seconds
     }
   });

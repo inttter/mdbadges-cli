a591674df4b308ed22b360ec185dc3ba8c45d811
Inter <hi@iinter.me>
2024-03-11 17:45:07 +0000

    ✨ feat(badges): add Solid

--- src/badges.js
+++ src/badges.js
@@ -245,6 +245,7 @@ const badges = {
         'redux': '[![Redux](https://img.shields.io/badge/Redux-764ABC?logo=redux&logoColor=fff)](#) ',
         'sass': '[![Sass](https://img.shields.io/badge/Sass-C69?logo=sass&logoColor=fff)](#) ',
         'shadcn/ui': '[![shadcn/ui](https://img.shields.io/badge/shadcn%2Fui-000?logo=shadcnui&logoColor=fff)](#) ',
+        'solid': '[![Solid](https://img.shields.io/badge/Solid-2C4F7C?logo=solid&logoColor=fff))](#) ',
         'spring-boot': '[![Spring Boot](https://img.shields.io/badge/Spring%20Boot-6DB33F?logo=springboot&logoColor=fff)](#) ',
         'strapi': '[![Strapi](https://img.shields.io/badge/Strapi-%232E7EEA.svg?logo=strapi&logoColor=white)](#) ',
         'symfony': '[![Symfony](https://img.shields.io/badge/Symfony-black?logo=symfony)](#) ',

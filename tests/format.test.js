fce94d327c654883048a0372e4340f20724c3a79
Inter <hi@iinter.me>
2024-05-29 16:08:04 +0100

    🧪 tests(format): update `escapeHtml` test to use new format

--- tests/format.test.js
+++ tests/format.test.js
@@ -6,31 +6,31 @@ const {
   } = require('../src/utils');
   
     describe('formatCategoryName', () => {
-      test('This formats category names that have dashes between the spaces', () => {
+      test('should format category names that have dashes between the spaces', () => {
         const result = formatCategoryName('app-store');
         expect(result).toBe('App Store');
       });
     });
 
     describe('formatBadgeName', () => {
-        test('This formats badge names that have dashes between the spaces', () => {
+        test('should format badge names that have dashes between the spaces', () => {
           const result = formatBadgeName('discord');
           expect(result).toBe('Discord');
         });
       });
   
     describe('SearchCategory', () => {
-      test('This converts categories to lowercase by adding hyphens', () => {
+      test('should convert categories to lowercase by adding hyphens', () => {
         const result = searchCategory('App Store');
         expect(result).toBe('app-store');
       });
     });
   
     describe('escapeHtml', () => {
-        test('This escapes special characters so that syntax is displayed correctly', () => {
-          const unsafeHtmlString = '<a href=""><img alt="![Discord" src="https://img.shields.io/badge/Discord-%235865F2.svg?&logo=discord&logoColor=white"></a>';
-          const expectedHtmlString = '&lt;a href=&quot;&quot;>&lt;img alt=&quot;![Discord&quot; src=&quot;https://img.shields.io/badge/Discord-%235865F2.svg?&amp;logo=discord&amp;logoColor=white&quot;>&lt;/a>';
+      test('should escape special characters in HTML string', () => {
+          const unsafeHtmlString = '<a href="https://discord.com">\n  <img src="https://img.shields.io/badge/Discord-%235865F2.svg?&logo=discord&logoColor=white" alt="Discord">\n</a>';
+          const expectedHtmlString = '&lt;a href=&quot;https://discord.com&quot;>\n  &lt;img src=&quot;https://img.shields.io/badge/Discord-%235865F2.svg?&amp;logo=discord&amp;logoColor=white&quot; alt=&quot;Discord&quot;>\n&lt;/a>';
           const result = escapeHtml(unsafeHtmlString);
           expect(result).toBe(expectedHtmlString);
-          });
-        });
+      });
+  });
